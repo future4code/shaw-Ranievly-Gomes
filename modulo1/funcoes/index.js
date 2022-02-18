564bd5cd85a50d3ea67b89f6a58d191ac167d1ec
RaniSales <Ranievly5@hotmail.com>
2022-02-17 22:05:02 -0300

    criado-pasta-funcoes-adicionado-arquivo-sobre-funcoes

---
+++ modulo1/funcoes/index.js
@@ -0,0 +1,130 @@
+/*QUESTÃO01
+
+Resposta a: 
+Vai ser impresso 10,50
+
+Resposta b: 
+Não apareceria no console.log o resulta
+
+*/
+//-----------------------------------------------------
+
+//QUESTÃO2
+
+/*
+
+Resposta a:
+primeiro vai coletar o texto do usuário. a função(outraFuncao) deixa o texto todo minúsculo
+e procura se no texto existe a palavra cenoura.Portanto o console.log irá retornar o texto minusculo e se é false ou true se existe cenoura no texto.
+
+Resposta b:
+true / eu gosto de cenoura
+true / cenoura é bom pra vista
+false / cenouras crescem na terra
+
+*/
+
+//---------------------------------------------------
+ 
+//QUESTÃO1
+/*
+function saudacao(eu){
+	console.log(eu)
+}
+saudacao("Eu sou Rani, tenho 22 anos, moro em Goiânia, e sou estudante da Labenu.")
+*/
+
+
+//---------------------------------------------------
+
+//QUESTAO2
+
+/*
+
+letra A:
+
+function soma (a, b){
+    return a + b
+}
+
+const mostrarConsole = soma (10,20)
+
+console.log(mostrarConsole)
+
+//Resposta 30
+
+*/
+
+// Letra B:
+
+/*
+let a = 5
+let b = 3
+
+function comparacaro(a, b) {
+        return a > b
+}
+console.log(comparacaro(a,b))
+
+*/
+
+// Letra C:
+
+/*
+let a = 8
+let b = 7
+
+function par(a, b) {
+        return (a % b) === 0
+}
+console.log(par(a))
+console.log(par(b))
+
+*/
+
+// QUESTÃO3
+
+/*
+function soma (a, b){
+    return a + b
+}
+
+function subtracao (a, b){
+    return a - b
+}
+
+function divisao (a, b){
+    return a / b
+}
+
+function multiplicacao (a, b){
+    return a * b
+}
+
+let numeroUm = Number(prompt("Digite um número:"))
+let numeroDois = Number(prompt("Digite outro número:"))
+
+console.log(`Números inseridos:${numeroUm} ${numeroDois}`)
+console.log("Soma:", soma(numeroUm,numeroDois))
+console.log("Subtracao:",subtracao(numeroUm,numeroDois))
+console.log("Divisao:", divisao(numeroUm,numeroDois))
+console.log("Multiplicacao:", multiplicacao(numeroUm,numeroDois))
+
+*/
+
+//-----------------------------------------------------------
+
+//DESAFIOS
+
+//QUESTÃO1
+
+//Letra A:
+/*
+let multiplicacao = (a,b) => { return(a*b) }
+
+let a = Number(prompt("Digite um número:"))
+let b = Number(prompt("Digite outro número:"))
+
+console.log("Resultado:", multiplicacao(a,b))
+
+*/
