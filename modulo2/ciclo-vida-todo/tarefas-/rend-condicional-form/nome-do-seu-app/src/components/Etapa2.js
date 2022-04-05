b114d263e632547459c6bf11ea0259a8ef816e6c
RaniSales <Ranievly5@hotmail.com>
2022-04-04 22:21:00 -0300

    postman

---
+++ modulo2/ciclo-vida-todo/tarefas-/rend-condicional-form/nome-do-seu-app/src/components/Etapa2.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+function EtapaDois() {
+  return (
+    <div className="EtapaUm">
+     <h1>Informações do Ensino Superior</h1>
+         <label>
+            5.Qual o curso?
+            <input type="text" name="name"/>
+         </label>
+
+         <label>
+            2.Qual a unidade de ensino?
+            <input type="text" name="name"/>
+         </label>
+         
+    </div>
+  );
+}
+
+export default EtapaDois;
